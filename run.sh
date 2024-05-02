30b88ef22024af6e89189cbc8db9fe05e795c1ec
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-05-02 15:37:02 -0500

    adding the schema loader for mysql

--- run.sh
+++ run.sh
@@ -8,5 +8,9 @@ if [ "$SCHEMA_TYPE" == "mongo" ]; then
   curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
   mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile global-bundle.pem --username $DOCDB_USER --password $DOCDB_PASS </app/schema/$COMPONENT.js
 
+fi
 
+
+if [ "$SCHEMA_TYPE" == "mysql" ]; then
+  mysql -h $DB_HOST -u$DB_USER -p$DB_PASS  </app/schema/$COMPONENT.sql
 fi
